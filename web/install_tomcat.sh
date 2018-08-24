d254f42a965568f166092f748bb28fad239ae408
Nalini Ganapati <nganapati@gmail.com>
2018-08-24 15:57:11 -0700

    Refine tomcat script

--- web/install_tomcat.sh
+++ web/install_tomcat.sh
@@ -10,4 +10,6 @@ echo "Run apache-tomcat-8.5.33/bin/startup.sh to start Tomcat"
 PORT=`grep "Connector port.*HTTP/1.1" server.xml | grep -o -E '[0-9]+' | head -1`
 echo "Connector Port is configured to $PORT for the Tomcat server"
 echo "   Forward Port=$PORT in Vagrantfile to a host port for monitoring the server"
+echo "   master.vm.network "forwarded_port", guest:$PORT, host:<Port #>"
+echo "   Restart vagrant with the command for the forwarded port to take effect - vagrant restart --provision"
 
